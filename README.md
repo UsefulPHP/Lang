eb6941add8b4b0e60788470e26b241536b107e65
Lauren Black <lauren@laurenblack.co.uk>
2020-06-14 15:04:49 +0100

    WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+# Useful Language Handler
+This is a WIP project.
+
+# Contributing
+Before contributing read [CONTRIBUTING.md](https://github.com/useful/useful/blob/master/CONTRIBUTING.md).
---
+++ src/Lang.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Useful;
+
+class Lang
+{
+    const FALLBACK = 'en_GB';
+    const DEFINITIONS = [
+        'en_GB' => __DIR__ . '/langDefs/en_GB.php'
+    ];
+    public static function get(string $key)
+    {
+
+    }
+}
