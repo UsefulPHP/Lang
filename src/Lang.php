ab5c5b8f457e037340f67c76e9060f6c7e5dcef8
Lauren Black <lauren@laurenblack.co.uk>
2020-06-15 14:34:54 +0100

    WIP

--- src/Lang.php
+++ src/Lang.php
@@ -5,12 +5,33 @@ namespace Useful;
 
 class Lang
 {
-    const FALLBACK = 'en_GB';
-    const DEFINITIONS = [
-        'en_GB' => __DIR__ . '/langDefs/en_GB.php'
-    ];
-    public static function get(string $key)
+    private static $locale;
+    private static $langFiles = [];
+    private static $lang = [];
+
+    public static function get(string $key): string
+    {
+        if(empty(self::$lang[static::class])){
+            
+        }
+    }
+
+    public static function getLocale(): string
+    {
+        if(empty(self::$locale)){
+            self::$locale = Locale::getDefault();
+        }
+        return self::$locale;
+    }
+
+    public static function setLocale(string $locale): void
+    {
+        self::$locale = $locale;
+    }
+
+    public static function registerTranslationFile(string $file, ?string $scope = null)
     {
 
     }
+
 }
